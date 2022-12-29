9d66fbb9e541dd840a477a535d714e8193d305c3
kamizinatulina <kamizinatulina@gmail.com>
2022-12-30 00:27:11 +0300

    junit4

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,21 +1,18 @@
-import org.testng.annotations.Test;
-import ru.netology.service.CashbackHackService;
 
-import static org.testng.Assert.assertEquals;
+import org.junit.Test;
+import ru.netology.service.CashbackHackService;
+import static org.junit.Assert.assertEquals;
+
 
 public class CashbackHackServiceTest {
-
     @Test
-    void remain() {
+    public void remain() {
         CashbackHackService service = new CashbackHackService();
-
-        int amount = 1001;
+        int amount = 1500;
 
         int actual = service.remain(amount);
-        int expected = 999;
+        int expected = 500;
         assertEquals(expected, actual);
-
-
     }
 
 }
