f498a519a6da974728d8c81b01bae409ff4b8f61
kamizinatulina <kamizinatulina@gmail.com>
2023-01-08 22:22:50 +0300

    Add failed test

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -14,5 +14,14 @@ public class CashbackHackServiceTest {
         int expected = 500;
         assertEquals(expected, actual);
     }
+    @Test
+    public void remainWhen1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
 
 }
